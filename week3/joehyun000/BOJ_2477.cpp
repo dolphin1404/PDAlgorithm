a36e29128834da241703be7fb2a578b72cd8b58b
Park-joehyun <joehyun000@naver.com>
2024-07-29 19:24:02 +0900

    ✨feat: BOJ_별찍기-10

--- week3/joehyun000/BOJ_2477.cpp
+++ week3/joehyun000/BOJ_2477.cpp
@@ -2,29 +2,35 @@
 #include <cmath>
 using namespace std;
 
-void star(int i, int j, int N) {
-    if(N == 3) {
-        if(i %3 ==1 && j%3 == 1) {
-            cout << " ";
-        }   
-        else {
+int N;
+
+// 재귀적으로 별 패턴을 채우는 함수
+void star(int x, int y, int n)
+{
+    // 현재 위치가 공백인지 확인
+    if ((x / n) % 3 == 1 && (y / n) % 3 == 1)
+        cout << " "; // 공백 출력
+    else
+    {
+        // 크기가 1이면 별 출력
+        if (n == 1)
             cout << "*";
-        }
+        else
+            // 크기를 3으로 나누어 재귀 호출
+            star(x, y, n / 3);
     }
- 
-     
 }
 
-int main() {
-    int k;
-    cin >> k;
+int main()
+{
+    cin >> N; // N 입력받기
 
-    int N = pow(3,k);
-
-    for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			star(i, j, N);
-		}
-		cout << '\n';
-	}
+    // N x N 크기의 패턴을 생성
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            // (i, j) 위치에서 시작하여 크기 N의 패턴을 출력
+            star(i, j, N);
+        cout << '\n'; // 한 행이 끝나면 줄바꿈
+    }
 }
